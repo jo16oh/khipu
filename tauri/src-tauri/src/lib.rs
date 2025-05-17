e6bad3f27b278dfaefeb38eac57abd3c98bd2384
Joichiro Hayashi <jyoichirou99@gmail.com>
2025-05-17 09:11:20 +0900

    Revert "remove tauri-plugin-opener"
    This reverts commit 7f0dd0eb90f44c3b3baccec96b14637958508e9a.

--- tauri/src-tauri/src/lib.rs
+++ tauri/src-tauri/src/lib.rs
@@ -9,6 +9,7 @@ fn greet(name: &str) -> String {
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     let app = tauri::Builder::default()
+        .plugin(tauri_plugin_opener::init())
         .plugin(
             tauri_plugin_window_state::Builder::default()
                 .with_state_flags(StateFlags::all() - StateFlags::VISIBLE)
