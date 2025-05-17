1c59fca68bf72dee6c13437b539947dcb94a0b3b
Joichiro Hayashi <jyoichirou99@gmail.com>
2025-05-17 09:19:09 +0900

    hide window titlebar for macos

--- tauri/src-tauri/src/lib.rs
+++ tauri/src-tauri/src/lib.rs
@@ -1,3 +1,4 @@
+use tauri::{TitleBarStyle, WebviewUrl, WebviewWindowBuilder};
 use tauri_plugin_window_state::{AppHandleExt, StateFlags};
 
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
@@ -17,6 +18,22 @@ pub fn run() {
         )
         .plugin(tauri_plugin_single_instance::init(|_, _, _| {}))
         .invoke_handler(tauri::generate_handler![greet])
+        .setup(|app| {
+            let win_builder = WebviewWindowBuilder::new(app, "main", WebviewUrl::default())
+                .hidden_title(true)
+                .inner_size(1025.0, 800.0)
+                .min_inner_size(470.0, 380.0)
+                .resizable(true)
+                .visible(true);
+
+            // set transparent title bar only when building for macOS
+            #[cfg(target_os = "macos")]
+            let win_builder = win_builder.title_bar_style(TitleBarStyle::Overlay);
+
+            win_builder.build()?;
+
+            Ok(())
+        })
         .build(tauri::generate_context!())
         .expect("error while running tauri application");
 
