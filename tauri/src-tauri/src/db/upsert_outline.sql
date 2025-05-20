16b3bd16403bdf2e1c5c8e95d0387ae7cdb18f40
Joichiro Hayashi <jyoichirou99@gmail.com>
2025-05-20 17:31:28 +0900

    remove unnecessary update from upsert_outline.sql

--- tauri/src-tauri/src/db/upsert_outline.sql
+++ tauri/src-tauri/src/db/upsert_outline.sql
@@ -14,11 +14,9 @@ VALUES
   (?, ?, ?, ?, ?, ?, ?, ?, ?)
 ON CONFLICT DO UPDATE
 SET
-  id = excluded.id,
   parent_id = excluded.parent_id,
   findex = excluded.findex,
   doc = excluded.doc,
-  created_at = excluded.created_at,
   updated_at = excluded.updated_at,
   hidden = excluded.hidden,
   collapsed = excluded.collapsed,
