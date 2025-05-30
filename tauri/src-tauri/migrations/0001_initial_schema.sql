ffccaec8c95c490f242f3da376545b2876b8766d
Joichiro Hayashi <jyoichirou99@gmail.com>
2025-05-30 11:43:27 +0900

    add a triggers to preserve deleted assets in deleted_assets table

--- tauri/src-tauri/migrations/0001_initial_schema.sql
+++ tauri/src-tauri/migrations/0001_initial_schema.sql
@@ -40,6 +40,87 @@ CREATE INDEX "IDX$outlines.root_id" ON outlines (root_id ASC);
 
 CREATE INDEX "IDX$outlines.full_findex" ON outlines (full_findex ASC);
 
+CREATE TABLE deleted_outlines (
+  id TEXT PRIMARY KEY NOT NULL,
+  parent_id TEXT,
+  findex TEXT NOT NULL,
+  type TEXT NOT NULL,
+  doc TEXT NOT NULL,
+  created_at INTEGER NOT NULL,
+  updated_at INTEGER NOT NULL,
+  collapsed INTEGER NOT NULL DEFAULT 0,
+  completed INTEGER NOT NULL DEFAULT 0
+) STRICT;
+
+CREATE INDEX "IDX$deleted_outlines.parent_id" ON outlines (parent_id);
+
+CREATE TABLE y_updates (
+  id TEXT PRIMARY KEY NOT NULL,
+  outline_id TEXT REFERENCES outlines (id) ON DELETE CASCADE NOT NULL,
+  data BLOB NOT NULL,
+  timestamp INTEGER NOT NULL
+) STRICT;
+
+CREATE INDEX "IDX$y_updates.outline_id" ON y_updates (outline_id);
+
+CREATE TABLE deleted_y_updates (
+  id TEXT PRIMARY KEY NOT NULL,
+  outline_id TEXT NOT NULL,
+  data BLOB NOT NULL,
+  timestamp INTEGER NOT NULL
+) STRICT;
+
+CREATE INDEX "IDX$deleted_y_updates.outline_id" ON deleted_y_updates (outline_id);
+
+CREATE TABLE outline_links (
+  id_from TEXT REFERENCES outlines (id) ON DELETE CASCADE NOT NULL,
+  id_to TEXT NOT NULL, -- implicitly referes to outlines(id)
+  type TEXT NOT NULL,
+  PRIMARY KEY (id_from, id_to)
+) STRICT;
+
+CREATE INDEX "IDX$outline_links.id_from" ON outline_links (id_from);
+
+CREATE INDEX "IDX$outline_links.id_to" ON outline_links (id_to);
+
+CREATE TABLE assets (
+  hash TEXT PRIMARY KEY NOT NULL, -- SHA-256 hash of the data
+  data BLOB NOT NULL
+) STRICT;
+
+CREATE TABLE outline_asset_rel (
+  outline_id TEXT REFERENCES outlines (id) ON DELETE CASCADE NOT NULL,
+  asset_hash TEXT REFERENCES assets (hash) ON DELETE CASCADE NOT NULL,
+  filename TEXT NOT NULL,
+  extension TEXT NOT NULL,
+  PRIMARY KEY (outline_id, asset_hash, filename, extension)
+) STRICT;
+
+CREATE TABLE deleted_assets (
+  hash TEXT PRIMARY KEY NOT NULL,
+  data BLOB NOT NULL
+) STRICT;
+
+CREATE TABLE deleted_outline_asset_rel (
+  outline_id TEXT NOT NULL,
+  asset_hash TEXT NOT NULL,
+  filename TEXT NOT NULL,
+  extension TEXT NOT NULL,
+  PRIMARY KEY (outline_id, asset_hash, filename, extension)
+) STRICT;
+
+-- # FTS
+CREATE VIRTUAL TABLE fts USING fts5 (
+  doc,
+  content = '', -- contentless table
+  tokenize = "trigram remove_diacritics 1"
+);
+
+-- Table for searching one or two character queries with term prefix matching
+-- To include the last one or two characters in the search target, meaningless 2 characters need to be added by application logic
+CREATE VIRTUAL TABLE fts_vocab USING fts5vocab (fts, ROW);
+
+-- # Triggers
 CREATE TRIGGER set_path_and_root_id AFTER INSERT ON outlines FOR EACH ROW BEGIN
 UPDATE outlines
 SET
@@ -181,39 +262,7 @@ WHERE
 
 END;
 
-CREATE TABLE deleted_outlines (
-  id TEXT PRIMARY KEY NOT NULL,
-  parent_id TEXT,
-  findex TEXT NOT NULL,
-  type TEXT NOT NULL,
-  doc TEXT NOT NULL,
-  created_at INTEGER NOT NULL,
-  updated_at INTEGER NOT NULL,
-  collapsed INTEGER NOT NULL DEFAULT 0,
-  completed INTEGER NOT NULL DEFAULT 0
-) STRICT;
-
-CREATE INDEX "IDX$deleted_outlines.parent_id" ON outlines (parent_id);
-
-CREATE TABLE y_updates (
-  id TEXT PRIMARY KEY NOT NULL,
-  outline_id TEXT REFERENCES outlines (id) ON DELETE CASCADE NOT NULL,
-  data BLOB NOT NULL,
-  timestamp INTEGER NOT NULL
-) STRICT;
-
-CREATE INDEX "IDX$y_updates.outline_id" ON y_updates (outline_id);
-
-CREATE TABLE deleted_y_updates (
-  id TEXT PRIMARY KEY NOT NULL,
-  outline_id TEXT NOT NULL,
-  data BLOB NOT NULL,
-  timestamp INTEGER NOT NULL
-) STRICT;
-
-CREATE INDEX "IDX$deleted_y_updates.outline_id" ON deleted_y_updates (outline_id);
-
-CREATE TRIGGER insert_into_trashbox BEFORE DELETE ON outlines FOR EACH ROW BEGIN
+CREATE TRIGGER before_delete_on_outlines BEFORE DELETE ON outlines FOR EACH ROW BEGIN
 INSERT INTO
   deleted_outlines
 SELECT
@@ -236,39 +285,39 @@ FROM
 WHERE
   outline_id = OLD.id;
 
+INSERT INTO
+  deleted_outline_asset_rel
+SELECT
+  *
+FROM
+  outline_asset_rel
+WHERE
+  outline_id = OLD.id;
+
 END;
 
-CREATE TABLE outline_links (
-  id_from TEXT REFERENCES outlines (id) ON DELETE CASCADE NOT NULL,
-  id_to TEXT NOT NULL, -- implicitly referes to outlines(id)
-  type TEXT NOT NULL,
-  PRIMARY KEY (id_from, id_to)
-) STRICT;
+CREATE TRIGGER before_delete_on_outline_asset_rel AFTER DELETE ON outline_asset_rel FOR EACH ROW WHEN NOT EXISTS (
+  SELECT
+    1
+  FROM
+    outline_asset_rel
+  WHERE
+    asset_hash = OLD.asset_hash
+  LIMIT
+    1
+) BEGIN
+INSERT INTO
+  deleted_assets (hash, data)
+SELECT
+  hash,
+  data
+FROM
+  assets
+WHERE
+  hash = OLD.asset_hash;
 
-CREATE INDEX "IDX$outline_links.id_from" ON outline_links (id_from);
+DELETE FROM deleted_assets
+WHERE
+  hash = OLD.asset_hash;
 
-CREATE INDEX "IDX$outline_links.id_to" ON outline_links (id_to);
-
-CREATE TABLE assets (
-  hash TEXT PRIMARY KEY NOT NULL, -- SHA-256 hash of the data
-  data BLOB NOT NULL
-) STRICT;
-
-CREATE TABLE outline_asset_rel (
-  outline_id TEXT REFERENCES outlines (id) ON DELETE CASCADE NOT NULL,
-  asset_hash TEXT REFERENCES assets (hash) ON DELETE CASCADE NOT NULL,
-  filename TEXT NOT NULL,
-  extension TEXT NOT NULL,
-  PRIMARY KEY (outline_id, asset_hash, filename, extension)
-) STRICT;
-
--- # FTS
-CREATE VIRTUAL TABLE fts USING fts5 (
-  doc,
-  content = '', -- contentless table
-  tokenize = "trigram remove_diacritics 1"
-);
-
--- Table for searching one or two character queries with term prefix matching
--- To include the last one or two characters in the search target, meaningless 2 characters need to be added by application logic
-CREATE VIRTUAL TABLE fts_vocab USING fts5vocab (fts, ROW);
+END;
