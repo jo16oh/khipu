39ccbb72cf469862356773cd18157540405aeb0c
Joichiro Hayashi <jyoichirou99@gmail.com>
2025-06-02 16:11:21 +0900

    apply formatting

--- tauri/src/App.tsx
+++ tauri/src/App.tsx
@@ -1,7 +1,7 @@
-import { useState } from "react";
+import "./App.css";
 import reactLogo from "./assets/react.svg";
 import { invoke } from "@tauri-apps/api/core";
-import "./App.css";
+import { useState } from "react";
 
 function App() {
   const [greetMsg, setGreetMsg] = useState("");
@@ -10,6 +10,7 @@ function App() {
   async function greet() {
     // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
     setGreetMsg(await invoke("greet", { name }));
+    await fetch("");
   }
 
   return (
@@ -17,13 +18,13 @@ function App() {
       <h1>Welcome to Tauri + React</h1>
 
       <div className="row">
-        <a href="https://vitejs.dev" target="_blank">
+        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
           <img src="/vite.svg" className="logo vite" alt="Vite logo" />
         </a>
-        <a href="https://tauri.app" target="_blank">
+        <a href="https://tauri.app" target="_blank" rel="noreferrer">
           <img src="/tauri.svg" className="logo tauri" alt="Tauri logo" />
         </a>
-        <a href="https://reactjs.org" target="_blank">
+        <a href="https://reactjs.org" target="_blank" rel="noreferrer">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
