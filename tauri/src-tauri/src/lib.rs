0a59d7bf09d8fd206210e2974314af54b0806b82
Joichiro Hayashi <jyoichirou99@gmail.com>
2025-06-02 16:41:34 +0900

    remove clear_unreferenced_deleted_assets from app startup because the db is not loaded yet when initializing the app

--- tauri/src-tauri/src/lib.rs
+++ tauri/src-tauri/src/lib.rs
@@ -40,9 +40,6 @@ pub fn run() {
             // initialize connection state
             app.manage(ConnectionState::new());
 
-            let app_handle = app.app_handle().clone();
-            tokio::spawn(commands::clear_unreferenced_deleted_assets(app_handle));
-
             Ok(())
         })
         .register_asynchronous_uri_scheme_protocol("bin", custom_protocol::handle_request)
