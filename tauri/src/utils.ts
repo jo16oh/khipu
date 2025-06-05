232644fbf23734d193b04c54574528e9884168cb
Joichiro Hayashi <jyoichirou99@gmail.com>
2025-06-05 22:26:45 +0900

    add uuidv7bs58 function

--- tauri/src/utils.ts
+++ tauri/src/utils.ts
@@ -1,3 +1,10 @@
+import bs58 from "bs58";
+import { uuidv7obj } from "uuidv7";
+
+export function uuidv7bs58() {
+  return bs58.encode(uuidv7obj().bytes);
+}
+
 export type FractionallyIndexedItem = {
   id: string;
   findex: string;
