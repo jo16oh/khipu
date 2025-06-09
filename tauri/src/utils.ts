220e7e269d91deef1c8b2d73e48117919ca62392
Joichiro Hayashi <jyoichirou99@gmail.com>
2025-06-09 16:48:53 +0900

    add uint8ArrayToBase64Async function

--- tauri/src/utils.ts
+++ tauri/src/utils.ts
@@ -4,3 +4,22 @@ import { uuidv7obj } from "uuidv7";
 export function uuidv7bs58() {
   return bs58.encode(uuidv7obj().bytes);
 }
+
+export function uint8ArrayToBase64Async(uint8Array: Uint8Array): Promise<string> {
+  return new Promise((resolve, reject) => {
+    const blob = new Blob([uint8Array], { type: "application/octet-stream" });
+    const reader = new FileReader();
+
+    reader.onload = () => {
+      // remove url prefix "data:application/octet-stream;base64,..."
+      const base64String = (reader.result as string).split(",")[1]!;
+      resolve(base64String);
+    };
+
+    reader.onerror = (error) => {
+      reject(error);
+    };
+
+    reader.readAsDataURL(blob);
+  });
+}
