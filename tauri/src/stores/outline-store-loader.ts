a080f9d2359fb7f994689ab464d05a49caa7d076
Joichiro Hayashi <jyoichirou99@gmail.com>
2025-06-13 09:18:19 +0900

    implement fetchTimeline method to OutlineStoreLoader

--- tauri/src/stores/outline-store-loader.ts
+++ tauri/src/stores/outline-store-loader.ts
@@ -1,8 +1,8 @@
-import { commands } from "generated/tauri-commands";
+import { OrderBy, TimelinePosition, commands } from "generated/tauri-commands";
 import { Outline } from "src/model";
 import { RegisterToStore } from "./outline-store";
 
-type Commands = Pick<typeof commands, "tree">;
+type Commands = Pick<typeof commands, "tree" | "timeline">;
 
 export class OutlineStoreLoader {
   readonly #registerToStore: RegisterToStore;
@@ -17,5 +17,15 @@ export class OutlineStoreLoader {
     const tree = await this.#commands.tree(id);
     const outlines = tree.map(Outline.from);
     this.#registerToStore(...outlines);
+  }
+
+  async fetchTimeline(position: TimelinePosition, order: OrderBy) {
+    const [dayStart, outlines]: [number, Outline[]] = await this.#commands
+      .timeline(position, order)
+      .then(({ dayStart, outlines }) => [dayStart, outlines.map(Outline.from)]);
+
+    this.#registerToStore(...outlines);
+
+    return outlines.length ? dayStart : null;
   }
 }
