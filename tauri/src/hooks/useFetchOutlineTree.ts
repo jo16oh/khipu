b78042d4ac714922a13e1d70c9964ddf4f876526
Joichiro Hayashi <jyoichirou99@gmail.com>
2025-06-15 18:24:39 +0900

    modify style of useFetchOutlineTree

--- tauri/src/hooks/useFetchOutlineTree.ts
+++ tauri/src/hooks/useFetchOutlineTree.ts
@@ -3,10 +3,6 @@ import { useOutlineStore } from "src/Providers";
 
 export function useFetchOutlineTree(id: string) {
   const store = useOutlineStore();
-
-  const fetchPromise = useMemo(async () => {
-    await store.loader.fetchTree(id);
-  }, [store.loader, id]);
-
+  const fetchPromise = useMemo(() => store.loader.fetchTree(id), [store.loader, id]);
   use(fetchPromise);
 }
