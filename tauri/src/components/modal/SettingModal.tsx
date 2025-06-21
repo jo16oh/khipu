f5cff74748efd54436ff315774c891346d0c8f08
Joichiro Hayashi <jyoichirou99@gmail.com>
2025-06-21 16:10:53 +0900

    scafollding SettingModal

---
+++ tauri/src/components/modal/SettingModal.tsx
@@ -0,0 +1,32 @@
+import { css } from "generated/styled-system/css";
+import { ReactNode } from "react";
+import { Button } from "react-aria-components";
+import Dialog from "../common/Dialog";
+
+export default function SettingModal({ trigger }: { trigger: ReactNode }) {
+  return (
+    <Dialog
+      trigger={trigger}
+      content={({ close }) => (
+        <>
+          <p>このダイアログはオーバーレイのクリックで閉じられます。</p>
+          <Button
+            className={css({
+              cursor: "pointer",
+              alignSelf: "flex-end",
+              borderRadius: "md",
+              marginTop: "4",
+              padding: "2",
+              color: "white",
+              backgroundColor: "blue.500",
+              _hover: { backgroundColor: "blue.600" },
+            })}
+            onPress={close}
+          >
+            Close
+          </Button>
+        </>
+      )}
+    />
+  );
+}
