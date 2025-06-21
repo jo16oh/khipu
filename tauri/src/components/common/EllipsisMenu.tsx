5e1ea55ea9a9ed8b9adf26d2c7a94c036dbd6ded
Joichiro Hayashi <jyoichirou99@gmail.com>
2025-06-21 16:07:14 +0900

    add EllipsisMenu component

---
+++ tauri/src/components/common/EllipsisMenu.tsx
@@ -0,0 +1,38 @@
+import { styled } from "generated/styled-system/jsx";
+import { circle, square } from "generated/styled-system/patterns";
+import { Ellipsis } from "lucide-react";
+import { ComponentProps } from "react";
+import { Button } from "react-aria-components";
+import Popover from "./Popover";
+
+const EllipsisMenu = (props: Omit<ComponentProps<typeof Popover>, "trigger">) => (
+  <Popover
+    trigger={
+      <StyledButton>
+        <EllipsisIcon />
+      </StyledButton>
+    }
+    {...props}
+  />
+);
+
+const StyledButton = styled(Button, {
+  base: circle.raw({
+    display: "grid",
+    rounded: "full",
+    size: "5",
+    color: "transparent",
+    fontSize: "md",
+    placeContent: "center",
+    _hover: { bg: "stone.200" },
+    _groupHover: { color: "stone.900" },
+  }),
+});
+
+const EllipsisIcon = styled(Ellipsis, {
+  base: square.raw({
+    size: "4",
+  }),
+});
+
+export default EllipsisMenu;
