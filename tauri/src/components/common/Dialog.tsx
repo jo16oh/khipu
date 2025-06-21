baec5d85e747906b308fb865970840412768f322
Joichiro Hayashi <jyoichirou99@gmail.com>
2025-06-21 16:06:51 +0900

    add Dialog component

---
+++ tauri/src/components/common/Dialog.tsx
@@ -0,0 +1,121 @@
+import { styled } from "generated/styled-system/jsx";
+import { ReactNode } from "react";
+import {
+  Dialog as DialogContent,
+  DialogRenderProps,
+  DialogTrigger,
+  DialogTriggerProps,
+  Modal,
+  ModalOverlay,
+} from "react-aria-components";
+
+const Dialog = ({
+  trigger,
+  triggerProps,
+  content,
+  contentProps,
+  buttons,
+  overlayProps,
+}: {
+  trigger: ReactNode;
+  triggerProps?: Omit<DialogTriggerProps, "children">;
+  content: (props: DialogRenderProps) => ReactNode;
+  contentProps?: Parameters<typeof StyledDialogContent>[0];
+  buttons?: ReactNode;
+  overlayProps?: Parameters<typeof StyledModalOverlay>[0];
+}) => (
+  <DialogTrigger {...triggerProps}>
+    {trigger}
+
+    <StyledModalOverlay
+      isDismissable
+      shouldCloseOnInteractOutside={(e) =>
+        e.attributes.getNamedItem("data-tauri-drag-region") ? false : true
+      }
+      {...overlayProps}
+    >
+      <StyledModal isDismissable>
+        {({ state }) => (
+          <StyledDialogContent {...contentProps}>
+            {(props) => (
+              <>
+                {buttons && <Buttons onClick={() => state.close()}>{buttons}</Buttons>}
+                {content(props)}
+              </>
+            )}
+          </StyledDialogContent>
+        )}
+      </StyledModal>
+    </StyledModalOverlay>
+  </DialogTrigger>
+);
+
+const StyledModalOverlay = styled(ModalOverlay, {
+  base: {
+    display: "grid",
+    zIndex: "200",
+    pos: "fixed",
+    top: "0",
+    left: "0",
+    w: "screen",
+    h: "screen",
+    bg: "stone.900/60",
+    placeContent: "center",
+    duration: "fast",
+    "&[data-entering]": {
+      fadeIn: "0",
+    },
+    "&[data-exiting]": {
+      fadeOut: "0",
+    },
+  },
+});
+
+const StyledModal = styled(Modal, {
+  base: {
+    "&[data-entering]": {
+      fadeIn: "0",
+      zoomIn: "0.75",
+    },
+    "&[data-exiting]": {
+      fadeOut: "0",
+      zoomOut: "0.75",
+    },
+  },
+});
+
+const StyledDialogContent = styled(DialogContent, {
+  base: {
+    zIndex: "300",
+    pos: "relative",
+    top: "[7px]",
+    ring: "none",
+    rounded: "lg",
+    w: "[calc(100vw - 5.5rem)]",
+    maxW: "3xl",
+    h: "fit",
+    maxH: "[calc(100vh - 42px)]",
+    bg: "stone.50",
+    shadow: "lg",
+    duration: "fast",
+  },
+});
+
+const Buttons = styled("div", {
+  base: {
+    display: "flex",
+    zIndex: "300",
+    pos: "absolute",
+    top: "0",
+    right: "-11",
+    gap: "2",
+    flexDir: "column",
+    justifyContent: "start",
+    alignItems: "end",
+    w: "11",
+    h: "fit",
+    p: "1",
+  },
+});
+
+export default Dialog;
