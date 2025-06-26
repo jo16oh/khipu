7d758c479b41fc1b344ef3dcbf672eaed33c5b14
Joichiro Hayashi <jyoichirou99@gmail.com>
2025-06-26 15:10:09 +0900

    scafollding HoverViewModal

---
+++ tauri/src/components/modal/HoverViewModal.tsx
@@ -0,0 +1,100 @@
+import { css } from "generated/styled-system/css";
+import { styled } from "generated/styled-system/jsx";
+import { center, square } from "generated/styled-system/patterns";
+import { ChevronLeft, ChevronRight, X } from "lucide-react";
+import { ReactNode, useRef } from "react";
+import { Button } from "react-aria-components";
+import { createViewStateStore } from "src/stores/view-state-store";
+import { useWorkspaceState } from "src/stores/workspace-state-store";
+import { useStore } from "zustand";
+import { useShallow } from "zustand/react/shallow";
+import Dialog from "../common/Dialog";
+import DialogSideActionButton, {
+  dialogSideActionButtonIconStyle,
+} from "../common/DialogSideButton";
+
+export default function HoverViewModal({ trigger }: { trigger: ReactNode }) {
+  const viewStateStore = useRef(createViewStateStore());
+
+  const viewState = useStore(viewStateStore.current);
+
+  const [hover, openHover, closeHover] = useWorkspaceState(
+    useShallow(({ hover, openHover, closeHover }) => [hover, openHover, closeHover]),
+  );
+
+  return (
+    <Dialog
+      trigger={trigger}
+      triggerProps={{
+        onOpenChange(isOpen) {
+          if (isOpen) {
+            openHover(viewStateStore.current);
+          } else {
+            closeHover();
+          }
+          console.log(hover);
+        },
+      }}
+      buttons={(state) => (
+        <DialogSideActionButton onClick={state.close}>
+          <X className={css(dialogSideActionButtonIconStyle)} />
+        </DialogSideActionButton>
+      )}
+      content={() => (
+        <ViewCotainer>
+          <ViewHeader>
+            <HistoryButton className="group" isDisabled={!viewState.hasPrevious()}>
+              <ChevronLeft className={headerIconStyle} />
+            </HistoryButton>
+            <HistoryButton className="group" isDisabled={!viewState.hasNext()}>
+              <ChevronRight className={headerIconStyle} />
+            </HistoryButton>
+          </ViewHeader>
+        </ViewCotainer>
+      )}
+    />
+  );
+}
+
+const ViewCotainer = styled("div", {
+  base: {
+    rounded: "md",
+    h: "[90vh]",
+    px: "1.5",
+    bg: "stone.50",
+  },
+});
+
+const ViewHeader = styled("div", {
+  base: {
+    display: "flex",
+    gap: "2",
+    alignItems: "center",
+    borderBottomWidth: "thin",
+    borderBottomColor: "stone.200",
+    p: "1",
+  },
+});
+
+const HistoryButton = styled(Button, {
+  base: center.raw({
+    rounded: "full",
+    p: "1",
+    _disabled: {
+      _hover: {
+        bg: "transparent",
+      },
+    },
+    _hover: {
+      bg: "stone.200",
+    },
+  }),
+});
+
+const headerIconStyle = square({
+  size: "4",
+  color: "stone.500",
+  _groupDisabled: {
+    color: "stone.300",
+  },
+});
