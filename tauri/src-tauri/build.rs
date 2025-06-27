f4d40fbd293c72bd7116e4e320a6b9e4ba193400
Joichiro Hayashi <jyoichirou99@gmail.com>
2025-06-27 21:41:23 +0900

    fix db path for sqlx

--- tauri/src-tauri/build.rs
+++ tauri/src-tauri/build.rs
@@ -9,9 +9,9 @@ fn main() {
 }
 
 fn prepare_sqlx() {
-    println!("cargo:rustc-env=DATABASE_URL=sqlite:.sqlx.graph");
+    println!("cargo:rustc-env=DATABASE_URL=sqlite:.sqlx.db");
 
-    let url = ".sqlx.graph";
+    let url = ".sqlx.db";
 
     if PathBuf::from(url).exists() {
         std::fs::remove_file(url).unwrap();
