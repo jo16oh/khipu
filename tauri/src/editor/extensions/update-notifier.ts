e6532bc8f45157af269cccc0e9393bae73fb1730
Joichiro Hayashi <jyoichirou99@gmail.com>
2025-06-30 20:34:03 +0900

    sync doc update on editor blur

--- tauri/src/editor/extensions/update-notifier.ts
+++ tauri/src/editor/extensions/update-notifier.ts
@@ -4,8 +4,8 @@ import { type DocUpdateNotifier } from "src/stores/doc-update-notifier";
 export function createUpdateNotifierExtension(outlineId: string, notifier: DocUpdateNotifier) {
   return Extension.create({
     name: "doc-update-notifier",
-    onUpdate(event) {
-      const doc = event.editor.getJSON();
+    onBlur({ editor }) {
+      const doc = editor.getJSON();
       notifier.notify(outlineId, doc as JSONContent);
     },
   });
