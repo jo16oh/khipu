9fdef9e4730566d4940feea4a7e680122a349b7e
Joichiro Hayashi <jyoichirou99@gmail.com>
2025-07-01 17:20:31 +0900

    useObservableRef hook

---
+++ tauri/src/hooks/useObservableRef.tsx
@@ -0,0 +1,30 @@
+import { useRef, useMemo } from "react";
+
+export function useObservableRef<T>(value: T) {
+  const ref = useRef<T>(value);
+  const subscribers = useRef<Set<(value: T) => void>>(new Set());
+
+  const refObj = useMemo(
+    () => ({
+      get current() {
+        return ref.current;
+      },
+
+      set current(value: T) {
+        ref.current = value;
+
+        for (const cb of subscribers.current) {
+          cb(ref.current);
+        }
+      },
+
+      listen(cb: (value: T) => void) {
+        subscribers.current.add(cb);
+        return () => subscribers.current.delete(cb);
+      },
+    }),
+    [ref, subscribers],
+  );
+
+  return refObj;
+}
