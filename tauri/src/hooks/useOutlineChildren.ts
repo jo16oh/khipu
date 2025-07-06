e8f21b40177d1638350c8d1e9c4e47129ee73d69
Joichiro Hayashi <jyoichirou99@gmail.com>
2025-07-06 12:21:09 +0900

    return FractionallyIndexedList from useOutlineChildren

--- tauri/src/hooks/useOutlineChildren.ts
+++ tauri/src/hooks/useOutlineChildren.ts
@@ -1,15 +1,11 @@
-import { memoize } from "es-toolkit";
-import { useMemo, useSyncExternalStore } from "react";
+import { useSyncExternalStore } from "react";
 import { useOutlineStore } from "src/stores/outline-store";
 
-export function useOutlineChildren(id: string): string[] {
+export function useOutlineChildren(id: string) {
   const store = useOutlineStore();
-
-  type Children = ReturnType<typeof store.getOutlineChildren> | null;
-  const extractIds = useMemo(() => memoize((c: Children) => c?.map(({ id }) => id) ?? []), []);
 
   return useSyncExternalStore(
     (cb) => store.subscribeToOutlineChildren(id, cb),
-    () => extractIds(store.getOutlineChildren(id) ?? null),
+    () => store.getOutlineChildren(id),
   );
 }
