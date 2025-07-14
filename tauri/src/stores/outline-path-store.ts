0fbb6c0fe66c9ec974bb4db248583d6c8a5af862
Joichiro Hayashi <jyoichirou99@gmail.com>
2025-07-14 22:08:07 +0900

    add memoization to OutlinePathStore

--- tauri/src/stores/outline-path-store.ts
+++ tauri/src/stores/outline-path-store.ts
@@ -1,15 +1,22 @@
+import { MemoizeCache, memoize } from "es-toolkit";
 import { OutlineStore } from "./outline-store";
 import { SubscribersMap } from "./subscribers-map";
 
 type Id = string;
 type Callback = () => void;
 
+type MemoizedGetPathImpl = ((id: Id) => string[] | undefined) & {
+  cache: MemoizeCache<string, string[] | undefined>;
+};
+
 export class OutlinePathStore {
   #store: OutlineStore;
   #subscribers = new SubscribersMap<string, []>();
+  #memoizedGetPath: MemoizedGetPathImpl;
 
   constructor(store: OutlineStore) {
     this.#store = store;
+    this.#memoizedGetPath = memoize((id: Id) => this.#getPathImpl(id));
   }
 
   subscribe(id: Id, cb: Callback) {
@@ -18,24 +25,27 @@ export class OutlinePathStore {
   }
 
   getPath(id: Id) {
-    if (this.#store.has(id)) {
-      const buf: string[] = [];
-      this.#getPathImpl(id, this.#store, buf);
-      return buf;
-    } else {
-      return undefined;
-    }
+    return this.#memoizedGetPath(id);
   }
 
-  #getPathImpl(id: Id, store: OutlineStore, buf: string[]) {
+  #getPathImpl(id: Id): string[] {
+    const buf: string[] = [];
+    if (this.#store.has(id)) {
+      this.#buildPath(id, this.#store, buf);
+    }
+    return buf;
+  }
+
+  #buildPath(id: Id, store: OutlineStore, buf: string[]) {
     const outline = store.getOutline(id);
     if (outline?.parentId) {
       buf.unshift(outline.parentId);
-      this.#getPathImpl(outline.parentId, store, buf);
+      this.#buildPath(outline.parentId, store, buf);
     }
   }
 
   notify(changedOutlineId: Id) {
+    this.#memoizedGetPath.cache.delete(changedOutlineId);
     this.#subscribers.notify(changedOutlineId);
     const children = this.#store.getOutlineChildren(changedOutlineId) ?? [];
     for (const { id } of children) {
