c96182ebb0499bd081556a327accfd3e2a099164
Joichiro Hayashi <jo16oh@gmail.com>
2025-12-30 21:25:56 +0900

    configure css-modules

--- packages/app/vite.config.ts
+++ packages/app/vite.config.ts
@@ -8,6 +8,15 @@ const host = process.env.TAURI_DEV_HOST;
 export default defineConfig(async () => ({
   plugins: [preact()],
 
+  // CSS Modules configuration
+  css: {
+    modules: {
+      localsConvention: 'camelCaseOnly',
+      scopeBehaviour: 'local',
+      generateScopedName: '[name]__[local]___[hash:base64:5]',
+    }
+  },
+
   // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
   //
   // 1. prevent Vite from obscuring rust errors
